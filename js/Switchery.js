687730eb16076cba5ebf40611cb83c67ba1eb45b
Alexander Vergara <54550004+Alexx4008@users.noreply.github.com>
2019-11-30 15:04:29 -0500

    Prototipo V1

---
+++ js/Switchery.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function() {
+    
+    // Switchery
+    var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+    $('.js-switch').each(function() {
+        new Switchery($(this)[0], $(this).data());
+    });
+});
+    
