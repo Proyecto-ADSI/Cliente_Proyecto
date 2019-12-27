ba47610f116bd62264bd4835878bd609728647cd
Alexander vergara <alexx4008@gmail.com>
2019-12-27 18:11:55 -0500

    Restablecer Contraseña

--- Controllers/GuardarEmpleado.js
+++ Controllers/GuardarEmpleado.js
@@ -26,7 +26,6 @@ let datos =
 
 };
 
-    console.log(datos);
 
 $.ajax({
 
@@ -37,7 +36,7 @@ $.ajax({
     // tipo de petición que se hace
     type:'post',
     // tipo de contenido
-    contenType: 'aplication/json',
+    contentType: 'aplication/json',
     // los datos que se envían
     data: JSON.stringify(datos),
     // No procesar datos
@@ -54,89 +53,6 @@ $.ajax({
 }).fail(error =>{
     console.log(error);
 });
-
-
-
-// let GuardarEmpleado = (Id_Empleado) =>
-// {
-// let DatosEmpleado =
-// {  
-// "Id_Usuario":Id_Empleado,    
-// "Documento":$("#Documento").val(),
-// "Nombre":$("#Nombre").val(),
-// "Apellido":$("#Apelldo").val(),
-// "Email":$("#Email").val(),
-// "Sexo":$("#Sexo").val(),
-// "Turno":$("#Turno").val()
-
-// };
-
-// console.log(DatosEmpleado)
-
-// $.ajax({
-
-//     // A donde va a ir a guardar
-//     url: `${URL}/empleado`,
-//     // lo que se queire recibir
-//     dataType: 'json',
-//     // tipo de petición que se hace
-//     type:'post',
-//     // tipo de contenido
-//     contenType: 'aplication/json',
-//     // los datos que se envían
-//     data: JSON.stringify(DatosEmpleado),
-//     // No procesar datos
-//     processData: false
-
-
-// // done -> capturar respuesta del servidor 
-// }).done(respuesta => {
-//     // La api devuelve un booleando -> revisar accion de crear en la api  
-//         alert("Se guardaron los datos");
-        
-
-// }).fail(error =>{
-//     console.log(error);
-// });
-// }
-
-
-// let usuario = $("#Usuario").val()
-// console.log(usuario)
-// $.ajax({
-
-//     url: `${URL}/usuario/${usuario}`,
-//     dataType: 'json',
-//     type: 'get'
-
-
-// }).done(respuesta =>{
-
-
-//     let Array = []; 
-
-//     Array = respuesta.data;
-
-//     //Capturamos datos de Usuario
-
-//     let Rol = parseInt(Array['Id_Rol']) 
-//     let Contrasena= Array['Contrasena']
-//     let Id = Array['Id_Usuarios']
-
-//     GuardarEmpleado(Id);
-
-//     console.log(Array)
-
-        
-// }).fail(error =>{
-
-//     alert ("No Existe");
-
-// });
-
-
-// console.log(DatosUsuario)
-
 
 
 }
--- Controllers/Login.js
+++ Controllers/Login.js
@@ -68,11 +68,7 @@ let Login = () =>{
                 
                 }
                 
-           }
-            
-
-
-               
+           }       
             
         }).fail(error =>{
 
@@ -80,6 +76,97 @@ let Login = () =>{
 
         });
 
+}
+
+let ValidarUsuario = () =>
+{
+    usuario = $("#Usuario_Recuperar").val()
+    
+
+    $.ajax({
+        url: `${URL}/Usuarios/ValidarUsuario/${usuario}`,
+        dataType: 'json',
+        type: 'get',
+    }).done(respuesta => {
+
+        if(!respuesta.data){
+
+
+            swal("¡Usuario no válido!","Verifica el usuario e intenta nuevamente", "danger");
+
+        }else{
+
+            let email = respuesta.data.Email
+
+            let emailInicio = email.substring(0,4)
+
+            let emailFin = email.split("@");
+
+            let emailSeguro = emailInicio + "****@" + emailFin[1];
+
+            swal({
+                title: "Enviar correo",
+                text: "Se enviarán instrucciones al correo " + emailSeguro + " para restablecer la contraseña.",
+                type: "info",
+                showCancelButton: true,
+                confirmButtonText: "Enviar",
+                cancelButtonText: "Cancelar",
+                closeOnConfirm: false,
+                showLoaderOnConfirm: true
+              }, function () {
+                setTimeout(function () {
+        
+                    $.ajax({
+        
+                        url: `${URL}/Usuarios/EnviarCorreo/${usuario}`,
+                        type: 'get',
+                        dataType: 'json'
+                
+                    }).done(respuesta =>{
+                        
+                        if(respuesta.data.ok)
+                        {   
+                            swal({
+                                title: "¡Correo enviado!",
+                                text: "Revisa el correo que se ha enviado",
+                                type: "success",
+                                showCancelButton: false,
+                                confirmButtonClass: "btn-info",
+                                confirmButtonText: "Cerrar",
+                                closeOnConfirm: false
+                              },
+                              function(){
+
+                                location.href = "Login.html";
+
+                              });
+
+
+                            // swal("¡Correo enviado!","Revisa el correo que se ha enviado", "success");
+        
+                        }else{
+                            swal("¡Correo NO enviado!","Ha ocurrido un error intenta nuevamente", "danger");
+                        }
+                        
+                    }).fail(error => {
+                
+                        console.error(error);
+                    });
+        
+              
+                }, 2000);
+              });
+
+        }
+
+    }).fail(error =>{
+        console.error(error);
+    })
+    
+
+
+
+    
 }
 
 
