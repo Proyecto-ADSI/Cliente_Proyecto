a429af017b3698826d673272721a3f087c4160c3
Alexander Vergara <alexx4008@gmail.com>
2020-04-19 14:48:52 -0500

    Eliminar clientes terminado

--- Controllers/Clientes/EliminarCliente.js
+++ Controllers/Clientes/EliminarCliente.js
@@ -1,11 +1,9 @@
 EliminarCliente = (Id_Cliente) => {
-
     $.ajax({
         url: `${URL}/Cliente/${Id_Cliente}`,
         type: 'delete',
         datatype: 'json',
         success: function (datos) {
-
             if(datos.data.Eliminar){
 
                 RecargarDataTable();
@@ -13,9 +11,9 @@ EliminarCliente = (Id_Cliente) => {
             }else{
 
                  swal({
-                    title: "Error al eliminar.",
+                    title: "No se puede eliminar.",
                     type: "error",
-                    text: "No se puede eliminar el cliente, debido a que está asociado a un registro de llamada o visita externa.",
+                    text: "El cliente está asociado a un registro de llamada o visita externa, por tanto no se puede eliminar, puede inhabilitarlo.",
                     showCancelButton: false,
                     confirmButtonColor: "#2F6885",
                     confirmButtonText: "Cerrar",
